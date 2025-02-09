4e53073465907f0b1f6c52bf0a2800529df9e7ea
dayeon <dayon0820@naver.com>
2025-02-10 00:59:47 +0900

    create footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./default/header";
+import Footer from "./default/footer";
 import Activites from "./activites/activites";
 import MainPage from "./main/main";
 import About from "./about/about";
@@ -12,13 +13,13 @@ function App() {
     <Router>
       <Header></Header>
       <Routes>
-        <Route path="/main" element={<MainPage />} />
+        <Route path="/" element={<MainPage />} />
         <Route path="/activities" element={<Activites />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <Footer></Footer>
     </Router>
   );
 }
 
 export default App;
-
