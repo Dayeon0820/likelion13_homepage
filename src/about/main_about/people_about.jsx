2f701068ab09b7bfa0d91b202b88f1c012c7b79d
dayeon <dayon0820@naver.com>
2025-02-17 19:18:21 +0900

    13기로 문구 변경

--- src/about/main_about/people_about.jsx
+++ src/about/main_about/people_about.jsx
@@ -7,7 +7,7 @@ import Footer from "../../default/footer";
 
 function PeopleAbout() {
   const title = "PEOPLE";
-  const subtitle = "멋쟁이사자처럼 at 국민대 12기 운영진";
+  const subtitle = "멋쟁이사자처럼 at 국민대 13기 운영진";
   const p = `2025년 13기 운영진을 소개합니다. 13기 운영진은 개발, 디자인, 홍보부, 총무 파트로 구성되어 있습니다.`;
   const [members, setMembers] = useState([]);
   useEffect(() => {
